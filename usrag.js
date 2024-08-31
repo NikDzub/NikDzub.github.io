ccf5d41ab58ea3cd5cf4ccdedac462da324c5bc4
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-08-31 12:41:29 +0300

    .

--- usrag.js
+++ usrag.js
@@ -1,21 +1,43 @@
-const token = '27266|iEgx5HPu7PX97DD2NcD0G7O5VI6lEuDJ4b4ThYIe343a8bf1';
+const agent = navigator.userAgent;
 const android_ua =
   'Mozilla/5.0 (Linux; Android 12; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36';
+const token = '27266|iEgx5HPu7PX97DD2NcD0G7O5VI6lEuDJ4b4ThYIe343a8bf1';
 const us_ip = '45.33.144.25';
 const apiUrl = 'https://cors-anywhere.herokuapp.com/unlockcontent.net/api/v2';
-const urlWithParams = `${apiUrl}?ip=${encodeURIComponent(
-  us_ip
-)}&user_agent=${encodeURIComponent(android_ua)}&ctype=1`;
+const root = document.querySelector('#root');
+root.style.display = 'block';
+
+let ip = '';
+
+// GET IP
+fetch('https://api.ipify.org?format=json')
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    ip = data.ip;
+  })
+  .catch((error) => {
+    console.error('There was a problem with the fetch operation:', error);
+  });
 
 const html_elem = document.querySelector('html');
 html_elem.addEventListener('click', () => {
-  alert(2);
+  alert(agent);
 });
 const links = document.querySelectorAll('a');
 links.forEach((link) => {
   link.href = 'https://example.com';
 });
+// GET IP
 
+// GET OFFER
+const urlWithParams = `${apiUrl}?ip=${encodeURIComponent(
+  us_ip
+)}&user_agent=${encodeURIComponent(android_ua)}&ctype=1`;
 fetch(urlWithParams, {
   method: 'GET', // or 'POST', 'PUT', 'DELETE', etc. depending on your use case
   headers: {
@@ -45,3 +67,4 @@ fetch(urlWithParams, {
   .catch((error) => {
     console.error('There was a problem with the fetch operation:', error);
   });
+// GET OFFER
