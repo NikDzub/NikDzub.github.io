4ca240929c324645feae7b792326369fb2759db4
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-09-03 13:07:28 +0300

    .

--- usrag.js
+++ usrag.js
@@ -14,53 +14,58 @@ const ua_info = document.querySelector('#user_agent');
 
 if (agent.indexOf('music') >= 0) {
   console.log('in tiktok');
-  root.style.display = 'none';
   alert('Please open in browser');
+  root.style.display = 'none';
 } else {
   // GET IP
+  let ip = '';
+
   fetch('https://api.ipify.org?format=json')
     .then((response) => {
       return response.json();
     })
     .then((data) => {
       console.log(data.ip);
-
-      const ip = data.ip;
-      const urlWithParams = `${apiUrl}?ip=${encodeURIComponent(
-        ip
-      )}&user_agent=${encodeURIComponent(agent)}&ctype=1`;
-
-      fetch(urlWithParams, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data.offers);
-
-          const filteredOffers = data.offers.filter((offer) =>
-            offer.name.toLowerCase().includes('coin')
-          );
-
-          const sortedOffers = filteredOffers.sort((a, b) => {
-            const payoutA = parseFloat(a.payout) || 0;
-            const payoutB = parseFloat(b.payout) || 0;
-            return payoutB - payoutA;
-          });
-
-          const links = document.querySelectorAll('a');
-          links.forEach((link) => {
-            link.href = sortedOffers[0].link;
-          });
-          // location.href = sortedOffers[0].link;
-        });
+      ip = data.ip;
     })
     .catch((error) => {
       console.error('There was a problem with the fetch operation:', error);
+    });
+
+  const urlWithParams = `${apiUrl}?ip=${encodeURIComponent(
+    ip
+  )}&user_agent=${encodeURIComponent(agent)}&ctype=1`;
+
+  console.log(urlWithParams);
+
+  fetch(urlWithParams, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => {
+      console.log(response.json());
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.offers);
+
+      const filteredOffers = data.offers.filter((offer) =>
+        offer.name.toLowerCase().includes('coin')
+      );
+
+      const sortedOffers = filteredOffers.sort((a, b) => {
+        const payoutA = parseFloat(a.payout) || 0;
+        const payoutB = parseFloat(b.payout) || 0;
+        return payoutB - payoutA;
+      });
+
+      const links = document.querySelectorAll('a');
+      links.forEach((link) => {
+        link.href = sortedOffers[0].link;
+      });
+      // location.href = sortedOffers[0].link;
     });
 }
